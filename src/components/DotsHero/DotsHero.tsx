445215cb6069226e6705c864205136a14302add7
Tomas Quinteros <tomyquinteros494@gmail.com>
2024-06-03 12:04:32 -0300

    responsive_hero

--- src/components/DotsHero/DotsHero.tsx
+++ src/components/DotsHero/DotsHero.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const DotsHero = () => {
     return (
-        <div className='grid grid-cols-12 gap-8 z-[999] justify-items-center md:gap-20'>
+        <div className='flex flex-col justify-center md:grid md:grid-cols-12 md:justify-items-center gap-8 z-[999] md:gap-20'>
             <div className='flex items-center gap-3 col-span-12 md:col-span-4'>
                 <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <circle cx="8.5" cy="8.5" r="8.5" fill="#95F214" />
