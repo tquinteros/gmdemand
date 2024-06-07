9b47a113a208c4f2b663c87f435933d5ff2515bd
Tomas Quinteros <tomyquinteros494@gmail.com>
2024-06-06 22:58:04 -0300

    fix_mobile

--- src/components/ClientsTemplate/ClientsTemplate.tsx
+++ src/components/ClientsTemplate/ClientsTemplate.tsx
@@ -9,9 +9,9 @@ const ClientsTemplate = () => {
                 <h2 className='text-5xl md:text-6xl mb-8 text-center nextfont uppercase'>Our happy clients:</h2>
             </div>
             <div className='p-4 md:p-0'>
-                <div className='grid grid-cols-12 p-5 container mx-auto bg-[#F5F5F5] lg:gap-8 gap-3 rounded-xl'>
+                <div className='grid grid-cols-12 p-5 container mx-auto bg-[#F5F5F5] lg:gap-8 gap-6 rounded-xl'>
                     <div className='col-span-12 md:col-span-6 flex flex-col gap-8 pt-8 rounded-lg bg-white'>
-                        <h3 className='text-2xl md:text-4xl px-4 md:px-10'>Working with GM has been a game-changer for our projects. Their exceptional quality of work combined with their incredibly fast turnaround time makes them the best design agency.</h3>
+                        <h3 className='text-2xl md:text-4xl px-4 md:px-10 text-cen'>Working with GM has been a game-changer for our projects. Their exceptional quality of work combined with their incredibly fast turnaround time makes them the best design agency.</h3>
                         <div className='mt-auto flex justify-between pl-10'>
                             <div className='flex justify-end pb-6 flex-col'>
                                 <h3 className='text-lg text-[#B990ED] font-extrabold'>KIM CURRIER</h3>
